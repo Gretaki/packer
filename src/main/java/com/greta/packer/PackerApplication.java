6bc3b648549742ee254210ba08c800574006cb8d
GretaKi <greta@lyrasolar.com>
2024-02-18 12:35:51 +0200

    Init

---
+++ src/main/java/com/greta/packer/PackerApplication.java
@@ -0,0 +1,13 @@
+package com.greta.packer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackerApplication.class, args);
+	}
+
+}
